151037d4dd0dc9eec863bfd84662dba82736290e
Juliette Vlieghe <juliette.vlieghe@gmail.com>
2024-01-19 10:48:46 +0100

    Barycenter heuristic

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
-#include <string>
+#include <bits/stdc++.h>
+#include <algorithm>
 
 using namespace std;
+
+const int N = 100;
+
+bool compareAverage(const pair<int, double>& a, const pair<int, double>& b) {
+    return a.second < b.second;
+}
 
 int main() {
     string line;
     int n0, n1, m;
+    vector<int> adj[N];
+   
 
     while (getline(cin, line)) {
         // Skip comment lines
@@ -15,22 +24,42 @@ int main() {
 
         // Parse p-line
         if (line[0] == 'p') {
-            string ocr;
-            cin >> ocr >> n0 >> n1 >> m;
+            std::stringstream ss(line);
+            string p, ocr;
+            ss >> p >> ocr >> n0 >> n1 >> m;
+            //cout << n0 << n1 << m;
             break;
         }
     }
 
     // Read the edges
+    int x, y;
     for (int i = 0; i < m; i++) {
-        int x, y;
         cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
 
-        // Process the edge (x, y)
-        // ...
+    // Barycenter heuristic
+    pair<int, double> averagePosition[N];
+    for (int i = n0 + 1 ; i <= n0 + n1; i++) {
+        //cout << "i " << i << endl; 
+        double sum = 0;
+        int count = 0;
+        
+        for (int neighbor : adj[i]) {
+            sum += neighbor;
+            count++;
+        }
+        // cout << sum / count << endl;
+        
+        averagePosition[i] = make_pair(i, sum / count);
+    }
 
-        // Print the edge (x, y)
-        cout << x << " " << y << endl;
+    sort(averagePosition + n0, averagePosition + n0 + n1, compareAverage);
+    
+    for (int i = n0 + 1 ; i <= n0 + n1; i++) {
+        cout << averagePosition[i].first << endl;
     }
 
     return 0;
