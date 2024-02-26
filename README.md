c45d916d7d15726659b5ab6b32c99f478740a47e
Juliette Vlieghe <juliette.vlieghe@gmail.com>
2024-02-26 17:55:45 +0100

    Updated statistics

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 
 To plot the degree distribution:
 ```
-python path_to_script/graphs_statistics.py path_to_your_graph.gr
+python path_to_script/graphs_statistics.py -f path_to_your_graph/filename.gr
+```
+
+To get the minimum and maximum degree of different graphs:
+```
+python path_to_script/graphs_statistics.py -p path_to_your_graph -r start end
 ```
--- src/graphs_statistics.py
+++ src/graphs_statistics.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -6,37 +7,69 @@ import seaborn as sns
 def read_graph(filename: str):
     graph = None
     with open(filename, "r") as f:
-        a = 0
-        b = 0
         edges = []
         for line in f:
             if line[0] == "p":
-                a, b = map(int, line.split(" ")[2:4])
+                n0, n1 = map(int, line.split(" ")[2:4])
             elif line[0] == "c":
                 pass
             else:
                 edges.append(list(map(int, line.split(" "))))
-                offset = np.bincount(edges[:, 0])
-    return np.array(edges), offset
+    return n0, n1, np.array(edges)
+
+def count_neighbours(edges, i, n0=0):
+    if i == 0:
+        return np.bincount(edges[:, i])[1:]
+    else:
+        return np.bincount(edges[:, i])[n0 + 1:]
 
 
-edges, _ = read_graph(sys.argv[1])
 
-# Plot
+parser = argparse.ArgumentParser(description='Print information on the degree of the vertices of a graph.')
+parser.add_argument("-p", "--path", help="use with range for filenames of name 'path/i.gr' with i in range")
+parser.add_argument(
+    "-r",
+    "--range",
+    help="use with path for filenames of name 'path/i.gr' with i in range",
+    metavar=("start", "end"),
+    type=int,
+    nargs=2,
+)
+parser.add_argument("-f", "--filename")
+
 layer = ["fixed", "free"]
 
-fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-axs[0].set_title("Fixed layer")
-axs[1].set_title("Free layer")
-plt.suptitle("Degree repartition")
+if __name__ == '__main__':
+    argument = parser.parse_args()
+    if argument.range:
+        start, end = argument.range
+        print(start, end)
+    if argument.path:
+        path = argument.path
+    if argument.filename:
+        filename = argument.filename
 
-for i in range(2):
-    count_neighbours = np.bincount(edges[:, i])
-    count_neighbours = count_neighbours[count_neighbours > 0]
-    sns.histplot(
-        data=count_neighbours,
-        ax = axs[i],
-    )
-    print("Minimum and maximum degrees in the {} layer:{}, {}".format(layer[i], min(count_neighbours), max(count_neighbours)))
+    if argument.path and argument.range:
+        print("Minimum and maximum degrees:")
+        for j in range (start, end):
+            n0, n1, edges = read_graph(path + "/{}.gr".format(j))
+            for i in range(2):
+                c = count_neighbours(edges, i, n0=n0)
+                print("{} {}:\t {} {}".format(j, layer[i], min(c), max(c)))
+            print()
 
-plt.show()
+    if argument.filename:
+        n0, n1, edges = read_graph(argument.filename)
+
+        fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+        axs[0].set_title("Fixed layer")
+        axs[1].set_title("Free layer")
+        plt.suptitle("Degree distribution")
+        for i in range(2):
+            c = count_neighbours(edges, i, n0=n0)
+            print(c)
+            sns.histplot(
+                data=c,
+                ax = axs[i],
+            )
+        plt.show()
