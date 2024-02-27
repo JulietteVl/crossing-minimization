249768b6a5dfd863ca90c6d148ac9c1d43ce436c
Juliette Vlieghe <juliette.vlieghe@gmail.com>
2024-02-27 13:23:57 +0100

    median heuristic added in library

--- src/library.h
+++ src/library.h
@@ -3,6 +3,8 @@
 using namespace std;
 
 int *barycenter_ordering(int n0, int n1, int *offset, pair<int, int> *edges);
+int *median_ordering(int n0, int n1, int *offset, pair<int, int> *edges);
+int *greedy_ordering(int n0, int n1, int *offset, pair<int, int> *edges);
 bool compare_first(const pair<int, double> &a, const pair<int, double> &b);
 bool compare_second(const pair<int, double> &a, const pair<int, double> &b);
 
@@ -38,6 +40,31 @@ int *barycenter_ordering(int n0, int n1, int *offset, pair<int, int> *edges)
     return order;
 }
 
+int *median_ordering(int n0, int n1, int *offset, pair<int, int> *edges)
+{
+    // return the free vertices ordered by the median of their neighbours.
+    pair<int, double> *median_position = new pair<int, double>[n1];
+    int* order = new int[n1];
+    for (int i = n0 + 1; i <= n0 + n1; i++)
+    {
+        int middle = (offset[i] + offset[i + 1] - 1) / 2;
+        if (offset[i] < offset[i + 1])
+            median_position[i - n0 - 1] = make_pair(i, edges[middle].second);
+        else
+            median_position[i - n0 - 1] = make_pair(i, 0);
+    }
+    // cout << "median position:" << endl;
+    // for (int i = 0; i < n1; i++){
+    //     cout << median_position[i].first << " " << median_position[i].second << endl;
+    // }
+    sort(median_position, median_position + n1, compare_second);
+    for (int i = 0; i < n1; i++)
+    {
+        order[i] = median_position[i].first;
+    }
+    return order;
+}
+
 bool compare_first(const pair<int, double> &a, const pair<int, double> &b)
 {
     if (a.first == b.first)
@@ -49,5 +76,8 @@ bool compare_first(const pair<int, double> &a, const pair<int, double> &b)
 
 bool compare_second(const pair<int, double> &a, const pair<int, double> &b)
 {
+    if (a.second == b.second){
+        return a.first < b.first;
+    }
     return a.second < b.second;
 }
