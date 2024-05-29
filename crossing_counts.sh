858838f2283d697e3704ec1050ade71433f64a2e
Juliette Vlieghe <52038457+JulietteVl@users.noreply.github.com>
2024-05-29 12:13:13 +0200

    Workflow tests performance on medium set (#17)

--- crossing_counts.sh
+++ crossing_counts.sh
@@ -1,32 +1,20 @@
-## Evaluate the plus-value of recursion
+## Evaluate the score on the medium set
 
 g++ src/main.cpp
 truncate -s 0 crossing_counts.txt
+truncate -s 0 sol.sol
 
-opt1="mg"
-opt2="mgr"
-echo $opt1 >> crossing_counts.txt
-echo $opt2 >> crossing_counts.txt
+opt="bmgr"
 
-# medium with greedy
-start=$(date +%s.%N);
 for i in `seq 1 60 `
 do
-    echo "medium set $i"
     # provided solution
     pc_crossings=$(pace2024verifier -c medium_test_set/instances/$i.gr medium_test_set/solutions/$i.sol)
-    echo $pc_crossings
-    # without recursion
-    ./a.out medium_test_set/instances/$i.gr medium_test_set/${opt1}_sol/$i.sol -$opt1
-    my_crossings=$(pace2024verifier -c medium_test_set/instances/$i.gr medium_test_set/${opt1}_sol/$i.sol)
-    echo $my_crossings
-    python -c "print($my_crossings/$pc_crossings)" >> crossing_counts.txt
     # our algo
-    ./a.out medium_test_set/instances/$i.gr medium_test_set/${opt2}_sol/$i.sol -${opt2}
-    my_crossings=$(pace2024verifier -c medium_test_set/instances/$i.gr medium_test_set/${opt2}_sol/$i.sol)
-    echo $my_crossings
+    ./a.out medium_test_set/instances/$i.gr sol.sol -${opt}
+    my_crossings=$(pace2024verifier -c medium_test_set/instances/$i.gr sol.sol)
     python -c "print($my_crossings/$pc_crossings)" >> crossing_counts.txt
+    python -c "print($my_crossings)" >> crossing_counts.txt
+    truncate -s 0 sol.sol
+
 done
-duration=$(echo "$(date +%s.%N) - $start" | bc)
-printf "Execution time, medium: %.1f seconds" $duration
-echo
