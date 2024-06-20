ed7f88a2afdf102facf5b8b1f1973e67a2244296
Juliette Vlieghe <52038457+JulietteVl@users.noreply.github.com>
2024-06-20 15:35:05 +0200

    Solver description + minor changes (#25)
    Added time test
    Added info in README

--- tests/crossing_counts.sh
+++ tests/crossing_counts.sh
@@ -2,17 +2,25 @@
 
 g++ ../src/main.cpp
 truncate -s 0 crossing_counts.txt
-truncate -s 0 sol.sol
+mkdir medium_test_set/test
+
+for i in `seq 1 60 `
+do
+    start=$(date +%s.%N);
+    # our algo
+    ./a.out medium_test_set/instances/$i.gr > medium_test_set/test/$i.sol
+    duration=$(echo "$(date +%s.%N) - $start" | bc)
+    python -c "print($duration)" >> crossing_counts.txt
+done
 
 for i in `seq 1 60 `
 do
     # provided solution
     pc_crossings=$(pace2024verifier -c medium_test_set/instances/$i.gr medium_test_set/solutions/$i.sol)
     # our algo
-    ./a.out medium_test_set/instances/$i.gr > sol.sol
-    my_crossings=$(pace2024verifier -c medium_test_set/instances/$i.gr sol.sol)
-    python -c "print($my_crossings/$pc_crossings)" >> crossing_counts.txt
+    ./a.out medium_test_set/instances/$i.gr > medium_test_set/test/$i.sol
+    my_crossings=$(pace2024verifier -c medium_test_set/instances/$i.gr medium_test_set/test/$i.sol)
+    python -c "print($pc_crossings/$my_crossings)" >> crossing_counts.txt
     python -c "print($my_crossings)" >> crossing_counts.txt
-    truncate -s 0 sol.sol
 
 done
